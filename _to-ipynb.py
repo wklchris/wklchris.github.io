2f8d12464eda522406c8a7b93ce21953534fcd5d
wklchris <wklchris@hotmail.com>
2017-01-18 05:00:20 -0800

    Cancel keyboard inputting. Reading from file will soon be supported.

--- _to-ipynb.py
+++ _to-ipynb.py
@@ -1,20 +1,25 @@
 import os, re
 import shutil
-import datetime
-import clipboard
 
-today = datetime.datetime.today()
-today = '{}-{:0>2d}-{:0>2d}'.format(today.year, today.month, today.day)
-fname = input("Input the file name (DO NOT include '.ipynb'):")
-date = input("Input the date YYYY-mm-dd of post (none means today):")
-if date != "":
-    today = date
+# Add Jekyll head strings
 
+fname = "R-clustering"
+date = "2017-01-18"
+
+headstr  = '---\n'
+headstr += 'layout: post\n'
+headstr += 'title: R语言学习与速查（聚类）\n'
+headstr += 'category: R\n'
+headstr += 'tag: R-learning\n'
+headstr += '---\n\n'
+
+
+# Main
 thepath = os.getcwd()
 ipynb_path = os.path.join(thepath, 'ipynb')
 ipynb_image_path = os.path.join(ipynb_path, r'{}_files'.format(fname))
 destination_path = os.path.join(thepath, r'assets/ipynb-images')
-post_path = os.path.join(thepath, r'_posts/{}.md').format(today + '-' + fname)
+post_path = os.path.join(thepath, r'_posts/{}.md').format(date + '-' + fname)
 
 # Convert ipynb to markdown; 
 os.system('jupyter nbconvert --to markdown ipynb/{}.ipynb'.format(fname))
@@ -26,6 +31,8 @@ os.rename(os.path.join(thepath, r'_posts/{}.md').format(fname), post_path)
 
 # Move the images under "/ipynb/<fname>_files" to "/assets/ipynb-images"
 def moveallfiles(origindir, destinationdir):
+    if ~os.path.exists(origindir):
+        return
     for file in os.listdir(origindir):
         originfile = os.path.join(origindir, file)
         destinationfile = os.path.join(destinationdir, file)
@@ -33,24 +40,54 @@ def moveallfiles(origindir, destinationdir):
         if os.path.isfile(destinationfile):
             os.remove(destinationfile)
         shutil.move(originfile, destinationfile)
+    # Delete the origin image path
+    shutil.rmtree(ipynb_image_path)
 
 moveallfiles(ipynb_image_path, destination_path)
-# Delete the origin image path
-shutil.rmtree(ipynb_image_path)
-
-# Replace the image link strings
-headstr  = '---\n'
-headstr += 'layout: post\n'
-headstr += 'title: R语言学习与速查（1. 入门）\n'
-headstr += 'category: R\n'
-headstr += 'tag: R-learning\n'
-headstr += '---\n\n'
 
 with open(post_path, 'r', encoding='utf8') as f:
     fstr = f.read()
 
+# Replace the image link strings
 fstr = re.compile(r'{}_files'.format(fname)).sub(r'https://wklchris.github.io/assets/ipynb-images', fstr)
 fstr = headstr + fstr
+
+# Convert HTML table to markdown table
+def transfertable(tablehtml):
+    tablehtml = re.compile(r'<table>').sub('', tablehtml)
+    tablehtml = re.compile(r'</tbody>[\n]</table>').sub('', tablehtml)
+
+    # Table head
+    tablehtml = re.compile(r'<tr><th>').sub(r'#', tablehtml)
+    tablehead = re.compile(r'<thead>[\S\s]*?</thead>').findall(tablehtml)
+    tablehead = tablehead[0]
+    # Headline
+    col_num = len(re.compile(r'</th>').findall(tablehead))
+    tablehtml = re.compile(r'<tbody>').sub('|' + ' --- |' * col_num, tablehtml)
+
+    headcontent = re.compile(r'(?<=>)[\S]*?(?=</th>)').findall(tablehead)
+    newhead = '| ' + ' | '.join(headcontent) + ' |'
+    tablehtml = re.compile(tablehead).sub(newhead, tablehtml)
+
+    # First column
+    firstcol = re.compile(r'(?<=\s)<tr>[\S\s]*?<td>').findall(tablehtml)
+    for cell in firstcol:
+        origincell = cell
+        cell = re.compile(r'<tr><th[^>]*?>').sub('| **', cell)
+        cell = re.compile(r'</th><td>').sub('** | ', cell)
+        tablehtml = re.compile('\t' + origincell).sub(cell, tablehtml)
+
+    # Table body
+    tablehtml = re.compile(r'</td></tr>').sub(' |', tablehtml)
+    tablehtml = re.compile(r'</th><td>').sub(' | ', tablehtml)
+    tablehtml = re.compile(r'</td><td>').sub(' | ', tablehtml)
+
+    return tablehtml
+
+tablehtmllst = re.compile(r'<table>[\s\S]*?</table>').findall(fstr)
+if tablehtmllst:
+    for table in tablehtmllst:
+        fstr = re.compile(table).sub(transfertable(table), fstr)
 
 os.remove(post_path)
 with open(post_path, 'w', encoding='utf8') as f:
